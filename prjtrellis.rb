ad69f063f4da42d5809db386cb1f3bba0a3cb45d
Yehowshua Immanuel <programmed4jesus@gmail.com>
2020-03-27 11:31:17 -0400

    Bottle for high sierra

--- prjtrellis.rb
+++ prjtrellis.rb
@@ -9,6 +9,7 @@ class Prjtrellis < Formula
     root_url "https://github.com/BracketMaster/homebrew-rtl/releases/download/v1.0/"
     cellar :any
     sha256 "acf7cf521850b3c1e2feb181ed6b30e9653d80b1c62ea3ed09d906e42e735172" => :catalina
+    sha256 "57220e6fda6a64052c8dd2541bcbd4e4b90666612ee5e99fe0768a7d83382e75" => :high_sierra
   end
 
   head do
