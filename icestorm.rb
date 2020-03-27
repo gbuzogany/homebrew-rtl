f26e8ba40099e311ee7cab820d8bb499576b67dc
Yehowshua Immanuel <programmed4jesus@gmail.com>
2020-03-27 11:38:21 -0400

    Bottle for High Sierra

--- icestorm.rb
+++ icestorm.rb
@@ -9,6 +9,7 @@ class Icestorm < Formula
     root_url "https://github.com/BracketMaster/homebrew-rtl/releases/download/v1.0"
     cellar :any
     sha256 "ee2586e0c0373c5a7aab8fd26896ac6a48d35f1325a681c7fbad6c6654d67287" => :catalina
+    sha256 "2f515f0b55eb6a5459dda2d87a5e36d95fcb14d1fde810892ba48b1244f6d2f7" => :high_sierra
   end
 
   head do
