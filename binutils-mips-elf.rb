cc39b0b8514215d56da89c27ffab957f118fda62
Yehowshua Immanuel <programmed4jesus@gmail.com>
2020-05-13 02:05:47 -0400

    Update binutils-mips-elf.rb

--- binutils-mips-elf.rb
+++ binutils-mips-elf.rb
@@ -5,6 +5,11 @@ class BinutilsMipsElf < Formula
     mirror "https://ftpmirror.gnu.org/binutils/binutils-2.34.tar.xz"
     sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
   
+  bottle do
+    root_url "https://github.com/BracketMaster/homebrew-rtl/releases/download/v1.0"
+    sha256 "1e141b49b9c8184f5b85519a02b364663fe7975b9f8090e7b4db3fce79eb7c86" => :high_sierra
+  end
+    
     uses_from_macos "zlib"
   
     def install
