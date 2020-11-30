ee83f9b045c7770d3728eb5bcf8fdf1f769e55b6
Yehowshua Immanuel <programmed4jesus@gmail.com>
2020-11-30 16:35:41 -0500

    Update python36.rb

--- python36.rb
+++ python36.rb
@@ -1,4 +1,4 @@
-class Python36 < Formula
+class python36 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz"
